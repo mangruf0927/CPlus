b035ace2c081a98bdfff959cfcb87b793a46e297
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2025-07-25 20:20:41 +0900

    IntQueue.cpp

--- 자료구조와 알고리즘/04_스택과_큐/IntQueue.cpp
+++ 자료구조와 알고리즘/04_스택과_큐/IntQueue.cpp
@@ -100,6 +100,19 @@ int Search(const IntQueue *q, int x)
     return -1;
 }
 
+int Search2(const IntQueue *q, int x)
+{
+    if(q->num <= 0) return -1;
+
+    for(int i = 0; i < q->num; i++)
+    {
+        int idx = (i + q->front) % q->max;
+        if(x == q->que[idx]) return i;
+    }
+
+    return -1;
+}
+
 // 모든 데이터 출력
 void Print(const IntQueue *q)
 {
