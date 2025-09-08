56ffe5d8940ad64e50367d93beaacb78d2c10a30
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2025-09-08 18:10:02 +0900

    [1316]그룹 단어 체커

--- algorithm-solutions/BAEKJOON/[1316]그룹_단어_체커.cpp
+++ algorithm-solutions/BAEKJOON/[1316]그룹_단어_체커.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
@@ -33,3 +34,42 @@ int main()
     cout << cnt << endl;
     return 0;
 }
+
+// set 사용
+/*
+int main()
+{
+    int N, cnt = 0;
+    cin >> N;
+    
+    vector<string> words(N);
+    
+    for(int i = 0; i < N; i++)
+        cin >> words[i];
+
+    for(int i = 0; i < N; i++)
+    {
+        set<char> seen;
+        char prev = 0;
+        bool isgroup = true;
+        
+        for(int j = 0; j < words[i].size(); j++)
+        {
+            if(words[i][j] != prev)
+            {
+                if(seen.count(words[i][j])) 
+                {
+                    isgroup = false;
+                    break;
+                }
+                seen.insert(words[i][j]);
+                prev = words[i][j];
+            }
+        }
+        if(isgroup) cnt++;
+    }
+
+    cout << cnt << endl;
+    return 0;
+}
+*/
