08171da4970c3deda967c7b868189481a5b897be
KimHanKyeol <123349413+mangruf0927@users.noreply.github.com>
2025-09-08 20:12:08 +0900

    [12909]올바른 괄호

---
+++ algorithm-solutions/PROGRAMMERS/[12909]올바른_괄호.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int cnt = 0;
+
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == '(') cnt++;
+        else if(s[i] == ')') cnt--;
+
+        if(cnt < 0) break;
+    }
+
+    if(cnt == 0) return true;
+    else return false;
+}
